35d2710b51061cde32ce1955e79b8c1a58b3970b
Thinkman Wang <wangxf1985@gmail.com>
2021-08-23 11:29:57 +0800

    update

---
+++ test/common/redis_test.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import redis
+from pythinkutils.redis.ThinkRedis import ThinkRedis
+from pythinkutils.redis.RedisLock import *
+from pythinkutils.common.datetime_utils import *
+from pythinkutils.common.log import g_logger
+from pythinkutils.common.StringUtils import *
+
+# if __name__ == '__main__':
+#     r = redis.StrictRedis(connection_pool=g_redis_pool)
+#     szID = acquire_lock_with_timeout(r, "migu_register_fxxk", acquire_timeout=10, lock_timeout=60)
+#     if False == is_empty_string(szID):
+#         print szID
+#     else:
+#         print "FXXK"
+
+def main():
+    r = redis.StrictRedis(connection_pool=ThinkRedis.get_conn_pool_ex())
+    r.set("FXXK", get_current_time_str())
+    szRet = r.get("FXXK")
+    g_logger.info(szRet)
+
+    szID = acquire_lock_with_timeout(r, "migu_register_fxxk", acquire_timeout=10, lock_timeout=60)
+    if False == is_empty_string(szID):
+        g_logger.info(szID)
+    else:
+        g_logger.info("FXXK")
+
+
+if __name__ == '__main__':
+    main()

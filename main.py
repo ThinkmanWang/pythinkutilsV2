7b34a78187d1f26996cf1542acad74d094f295b5
Thinkman Wang <wangxf1985@gmail.com>
2021-08-25 13:27:37 +0800

    update

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from pythinkutils.common.log import g_logger
+
 def main():
-    print("Hello World")
+    g_logger.info("Hello World")
 
 if __name__ == '__main__':
     main()

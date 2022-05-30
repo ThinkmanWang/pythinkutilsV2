6168814dda0f8840c4c8166a71c44a10edfc6f46
Thinkman Wang <wangxf1985@gmail.com>
2022-05-30 14:46:11 +0800

    update

--- test/aio/rabbit-pub-sub/aiorabit-pub.py
+++ test/aio/rabbit-pub-sub/aiorabit-pub.py
@@ -20,17 +20,20 @@ async def main() -> None:
             "fxxk", ExchangeType.FANOUT,
         )
 
-        message_body = get_current_time_str().encode()
+        while True:
+            message_body = get_current_time_str().encode()
 
-        message = Message(
-            message_body,
-            delivery_mode=DeliveryMode.PERSISTENT,
-        )
+            message = Message(
+                message_body,
+                delivery_mode=DeliveryMode.PERSISTENT,
+            )
 
-        # Sending the message
-        await logs_exchange.publish(message, routing_key="info")
+            # Sending the message
+            await logs_exchange.publish(message, routing_key="info")
 
-        print(f" [x] Sent {message!r}")
+            print(f" [x] Sent {message!r}")
+
+            await asyncio.sleep(1)
 
 
 if __name__ == "__main__":
